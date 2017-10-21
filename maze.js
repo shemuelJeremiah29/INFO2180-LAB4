b45815b7d293295394ce48d37837fa52940e47b5
shemuelJeremiah29 <drewmori17@gmail.com>
2017-10-20 23:46:47 -0500

    Exercise 6 Solution

--- maze.js
+++ maze.js
@@ -29,7 +29,7 @@ window.onload = function (){
 
  } 
 
-/*
+
  //Exercise 3 Solution.... 
  window.onload = function (){  
 
@@ -67,9 +67,9 @@ window.onload = function (){
 
  	
 
- }   */ 
+ }   
 
-/*
+
 //Exercise 4 Solution...
  window.onload = function(){ 
 
@@ -116,7 +116,6 @@ window.onload = function (){
   	}); 
 
  } 
- */ 
 
  //Exercise 5 Solution...
 window.onload = function (){ 
@@ -163,6 +162,77 @@ let touchWall = false;
   	}); 
 
 
+
+
+} 
+
+
+//Exercise 6 Solution...
+window.onload = function (){ 
+
+let touchWall = false; 
+
+ 
+ 	let walls= document.querySelectorAll("#maze div.boundary");   
+ 	let status= document.getElementById("status");
+	
+	
+ 		for (var i=0; i<walls.length; i++){
+
+ 			walls[i].addEventListener("mouseover", function(){ 
+ 				
+ 				for (var x=0; x<walls.length; x++){ 
+ 				walls[x].classList.add("youlose");   
+ 				
+ 				 
+ 				}     
+ 				
+ 				status.innerHTML="You Lose!!!"; 
+ 				touchWall = true; 
+ 			}); 
+          
+ 		}   
+
+
+ 	let finishFlag =false; 
+ 	let finishM = document.querySelector("#maze #end");  
+ 	finishM.addEventListener("mouseover", function (){  
+ 		finishFlag=true; 
+ 		if (touchWall ===true ){ 
+ 			status.innerHTML="No cheating!!!";  
+
+ 		}else{ 
+ 			status.innerHTML="You Win!!!";
+ 		}
+ 		 	
+ 		
+ 	});  
+
+ 	let clickStart=false; 
+ 	let start = document.getElementById("start");  
+ 	let wall=document.getElementsByClassName("boundary");  
+
+ 		start.addEventListener("click", function() { 
+		location.reload(); 
+		clickStart=true; 
+
+  	}); 
+
+ 	let mouseout= document.getElementById("maze");  
+ 	let mouseoutQ=document.querySelectorAll("#maze div.boundary"); 
+ 	mouseout.onmouseout = function (){ 
+
+ 		if (clickStart===true && finishFlag===true ){ 
+ 			for (var t= 0; t<mouseoutQ.length; t++){ 
+ 				mouseoutQ[i].classList.add("youlose"); 
+ 			}
+ 			status.innerHTML="You LOSE!!! CHEATER"; 
+ 		}else{ 
+ 			
+ 		}
+
+
+ 	}
 
 
 }
